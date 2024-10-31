44364bc384a83f3a5eb068f47850734808ddbf56
Fran <franciscodominguezllorens@gmail.com>
2024-10-31 19:21:56 +0100

    finished project

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
@@ -21,8 +20,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import {unstable_noStore as noStore } from "next/cache";
-
+import { unstable_noStore as noStore } from "next/cache";
 
 export default function NewWorkRoute() {
   noStore();
@@ -33,19 +31,16 @@ export default function NewWorkRoute() {
   const [selectedIdioma, setSelectedIdioma] = useState("en"); // Estado para el idioma
   const router = useRouter();
 
-
   // Función para añadir nuevos autores dinámicamente
   const handleAddAutor = () => {
     setAutores([...autores, ""]);
   };
-
 
   // Función para eliminar autores
   const handleRemoveAutor = (index: number) => {
     const newAutores = autores.filter((_, i) => i !== index);
     setAutores(newAutores);
   };
-
 
   // Función para actualizar el valor de los autores
   const handleAutorChange = (index: number, value: string) => {
@@ -54,20 +49,19 @@ export default function NewWorkRoute() {
     setAutores(newAutores);
   };
 
-
   // Función para generar el PDF con jsPDF dependiendo del idioma seleccionado
   const generatePDF = (content: string, title: string, autores: string) => {
     const doc = new jsPDF();
     const pageHeight = doc.internal.pageSize.height;
     const pageWidth = doc.internal.pageSize.width;
     const margin = 20;
- 
+
     // Variables de acuerdo con el idioma seleccionado
     let indice = "ÍNDICE";
     let contenido = "CONTENIDO";
     let bibliografia = "BIBLIOGRAFÍA";
     let autor = "Autor";  // Nueva variable para Autor
- 
+
     if (selectedIdioma === "en") {
       indice = "INDEX";
       contenido = "CONTENT";
@@ -89,14 +83,14 @@ export default function NewWorkRoute() {
       bibliografia = "BIBLIOGRAPHIE";
       autor = "Autor";  // Traducción en alemán (igual que en español)
     }
- 
+
     // Calcular la posición en el centro vertical
-    let titleFontSize = 40;
+    const titleFontSize = 40;
     const titleHeight = titleFontSize * 1.5;
     let yPosition = (pageHeight - titleHeight) / 2;
     doc.setFont("helvetica", "bold");
     doc.setFontSize(50);
- 
+
     // Título centrado
     const wrappedTitle: string[] = doc.splitTextToSize(title.toUpperCase(), pageWidth - margin * 2);
     wrappedTitle.forEach((line: string) => {
@@ -108,12 +102,12 @@ export default function NewWorkRoute() {
       yPosition += 20;
     });
     yPosition += 10;
- 
+
     // Autor centrado en la página
     doc.setFont("helvetica", "normal");
     doc.setFontSize(14);
     doc.setTextColor(128, 128, 128);
- 
+
     const wrappedAuthor: string[] = doc.splitTextToSize(`${autor}: ${autores}`, pageWidth - margin * 2);
     wrappedAuthor.forEach((line: string) => {
       if (yPosition + 10 > pageHeight - margin) {
@@ -123,103 +117,19 @@ export default function NewWorkRoute() {
       doc.text(line, pageWidth / 2, yPosition, { align: "center" });
       yPosition += 10;
     });
- 
+
     // Restablecer color del texto a negro
     doc.setTextColor(0, 0, 0);
- 
-    // Página 2: Índice
-    const index = content.match(new RegExp(`###${indice}###([\\s\\S]+?)###${contenido}###`))?.[1];
-    if (index) {
-      doc.addPage();
-      yPosition = margin;
- 
-      // Añadir título centrado del índice
-      doc.setFont("helvetica", "bold");
-      doc.setFontSize(21);
-      doc.text(indice, pageWidth / 2, yPosition, { align: "center" });
-      yPosition += 10;
- 
-      // Añadir el contenido del índice
-      doc.setFont("helvetica", "normal");
-      doc.setFontSize(12);
-      const indexLines: string[] = doc.splitTextToSize(index, pageWidth - margin * 2);
-      indexLines.forEach((line: string) => {
-        if (yPosition + 10 > pageHeight - margin) {
-          doc.addPage();
-          yPosition = margin;
-        }
-        doc.text(line, margin, yPosition, { align: "justify" });
-        yPosition += 10;
-      });
-    }
- 
-    // Página 3: Contenido
-    const bodyContent = content.match(new RegExp(`###${contenido}###([\\s\\S]+?)###${bibliografia}###`))?.[1];
-    if (bodyContent) {
-      doc.addPage();
-      yPosition = margin;
-      const bodyLines: string[] = bodyContent.split("\n");
-      bodyLines.forEach((line: string) => {
-        if (line.startsWith("###")) {
-          doc.setFont("helvetica", "bold");
-          doc.setFontSize(24); // Títulos principales
-        } else if (line.startsWith("##")) {
-          doc.setFont("helvetica", "bold");
-          doc.setFontSize(19); // Subtítulos
-        } else {
-          doc.setFontSize(12);
-          doc.setFont("helvetica", "normal"); // Texto normal
-        }
- 
-        const wrappedLines: string[] = doc.splitTextToSize(line.replace(/###|##/g, ""), pageWidth - margin * 2);
-        wrappedLines.forEach((wrappedLine: string) => {
-          if (yPosition + 10 > pageHeight - margin) {
-            doc.addPage();
-            yPosition = margin;
-          }
-          doc.text(wrappedLine, margin, yPosition, { align: "justify" });
-          yPosition += 10;
-        });
-      });
-    }
- 
-    // Última página: Bibliografía
-    const bibliography = content.match(new RegExp(`###${bibliografia}###([\\s\\S]+)`))?.[1];
-    if (bibliography) {
-      doc.addPage();
-      yPosition = margin;
- 
-      // Añadir título centrado
-      doc.setFont("helvetica", "bold");
-      doc.setFontSize(21);
-      doc.text(bibliografia, pageWidth / 2, yPosition, { align: "center" });
-      yPosition += 10;
- 
-      // Añadir contenido de la bibliografía
-      doc.setFont("helvetica", "normal");
-      doc.setFontSize(11);
-      const bibliographyLines: string[] = doc.splitTextToSize(bibliography, pageWidth - margin * 2);
-      bibliographyLines.forEach((line: string) => {
-        if (yPosition + 10 > pageHeight - margin) {
-          doc.addPage();
-          yPosition = margin;
-        }
-        doc.text(line, margin, yPosition, { align: "justify" });
-        yPosition += 10;
-      });
-    }
- 
+
     // Guardar el PDF generado
     const sanitizedTitle = title.replace(/[^a-zA-Z0-9 ]/g, ""); // Elimina caracteres especiales
     const fileName = `${sanitizedTitle}.pdf`; // Genera el nombre del archivo
     doc.save(fileName);
   };
- 
 
   // Función para manejar el envío del formulario
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    
 
     setLoading(true);
     setProgress(0); // Inicializa el progreso en 0 cuando comienza la acción
