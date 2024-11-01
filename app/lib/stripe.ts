f16468bcbdd442f79cef7c31a7623be9b0d9224d
Fran <franciscodominguezllorens@gmail.com>
2024-11-01 19:12:33 +0100

    added new env

--- app/lib/stripe.ts
+++ app/lib/stripe.ts
@@ -24,8 +24,8 @@ export const getStripeSession = async ({
       address: "auto",
       name: "auto",
     },
-    success_url: `${domainUrl}/payment/success`,
-    cancel_url: `${domainUrl}/payment/cancelled`,
+    success_url: `https://quickpaperai.com/payment/success`,
+    cancel_url: `https://quickpaperai.com/payment/cancelled`,
   });
   return session.url as string;
 };
